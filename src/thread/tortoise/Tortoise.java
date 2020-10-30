2ed5364639a72e89adf9c2273bc9f3380cad44a9
Pratik Sharma <pratik.sharma_cs19@gla.ac.in>
2020-10-30 21:31:12 +0530

    create a class Tortoise in thread package and extend it from Thread class.

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -1,4 +1,15 @@
 package thread.tortoise;
+import javax.swing.*;
 
-public class Tortoise {
+public class Tortoise extends Thread{
+    private static final int MILESTONES = 5;
+    private final Thread tortoise;
+    public Tortoise(){
+        this.tortoise = new Thread(this,"Tortoise");
+
+    }
+    public Thread getTortoise(){
+        return tortoise;
+    }
+
 }
